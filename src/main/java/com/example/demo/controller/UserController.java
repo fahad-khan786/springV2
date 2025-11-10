c5e99551808dc5d145d0d4641693c2c3dd2f0d73
Fahad Khan <fahad_khan@ildc-inlp.idealake.com>
2025-11-10 14:43:21 +0530

    changes done in hello controller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -46,7 +46,7 @@ public class UserController {
 
     @GetMapping(value = "/hello")
     public String getValue(){
-        return "Hi, i am from india";
+        return "Hi, i am fahad";
     }
 
 }
